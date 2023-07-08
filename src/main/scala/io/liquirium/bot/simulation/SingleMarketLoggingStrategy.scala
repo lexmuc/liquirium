20432919f1eddf77bdadea9b0989240133bc6509
Alexander Steinhoff <github-liquirium@alexander-steinhoff.net>
2023-07-08 15:07:49 +0200

    removed logging strategy, logging becomes simpler

--- src/main/scala/io/liquirium/bot/simulation/SingleMarketLoggingStrategy.scala
+++
@@ -1,5 +0,0 @@
-package io.liquirium.bot.simulation
-
-import io.liquirium.bot.SingleMarketStrategyBot
-
-trait SingleMarketLoggingStrategy[L <: SimulationLogger[L]] extends (SingleMarketStrategyBot => L)
--- src/main/scala/io/liquirium/bot/simulation/package.scala
+++ src/main/scala/io/liquirium/bot/simulation/package.scala
@@ -6,41 +6,37 @@ import io.liquirium.eval._
 
 package object simulation {
 
-  def singleMarketVisualizationLoggingStrategy(
+  def singleMarketVisualizationLogger(
+    bot: SingleMarketStrategyBot,
     additionalCandleStartMetrics: Map[String, Eval[BigDecimal]] = Map.empty,
     additionalCandleEndMetrics: Map[String, Eval[BigDecimal]] = Map.empty,
-  ) : SingleMarketLoggingStrategy[VisualizationLogger] =
-    new SingleMarketLoggingStrategy[VisualizationLogger] {
+  ): VisualizationLogger = {
+    val market = bot.runConfiguration.market
+    val startTime = bot.runConfiguration.startTime
+    val strategy = bot.strategy
+    // load more candles since indicators need some candle history
+    val candlesForIndicatorsEval =
+      InputEval(CandleHistoryInput(market, strategy.candleLength, startTime.minusSeconds(3600 * 10)))
 
-      override def apply(bot: SingleMarketStrategyBot): VisualizationLogger = {
-        val market = bot.runConfiguration.market
-        val startTime = bot.runConfiguration.startTime
-        val strategy = bot.strategy
-        // load more candles since indicators need some candle history
-        val candlesForIndicatorsEval =
-          InputEval(CandleHistoryInput(market, strategy.candleLength, startTime.minusSeconds(3600 * 10)))
+    val lastPriceEval = candlesForIndicatorsEval.map(_.lastPrice.get)
 
-        val lastPriceEval = candlesForIndicatorsEval.map(_.lastPrice.get)
+    val visualizationCandlesEval =
+      CandleAggregationFold.aggregate(
+        InputEval(CandleHistoryInput(market, strategy.candleLength, startTime)),
+        strategy.candleLength multipliedBy (12 * 4),
+      )
 
-        val visualizationCandlesEval =
-          CandleAggregationFold.aggregate(
-            InputEval(CandleHistoryInput(market, strategy.candleLength, startTime)),
-            strategy.candleLength multipliedBy (12 * 4),
-          )
-
-        VisualizationLogger(
-          candlesEval = visualizationCandlesEval,
-          candleStartEvals = Map(
-            "lastPrice" -> lastPriceEval,
-            "lowestSell" -> LowestSellEval(market, fallback = lastPriceEval),
-            "highestBuy" -> HighestBuyEval(market, fallback = lastPriceEval),
-          ) ++ additionalCandleStartMetrics,
-          candleEndEvals = Map(
-            "ownTradeVolume" -> LatestCandleTradeVolumeEval(visualizationCandlesEval, bot.tradeHistoryEval),
-          ) ++ additionalCandleEndMetrics,
-        )
-
-      }
-    }
+    VisualizationLogger(
+      candlesEval = visualizationCandlesEval,
+      candleStartEvals = Map(
+        "lastPrice" -> lastPriceEval,
+        "lowestSell" -> LowestSellEval(market, fallback = lastPriceEval),
+        "highestBuy" -> HighestBuyEval(market, fallback = lastPriceEval),
+      ) ++ additionalCandleStartMetrics,
+      candleEndEvals = Map(
+        "ownTradeVolume" -> LatestCandleTradeVolumeEval(visualizationCandlesEval, bot.tradeHistoryEval),
+      ) ++ additionalCandleEndMetrics,
+    )
+  }
 
 }
