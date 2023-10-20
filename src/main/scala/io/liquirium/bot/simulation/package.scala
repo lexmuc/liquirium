3161062b3c7ca05a720ab2ec6e985bfe167019da
Alexander Steinhoff <github-liquirium@alexander-steinhoff.net>
2023-10-20 06:14:11 +0200

    made visualization logger independent of bot class

--- src/main/scala/io/liquirium/bot/simulation/package.scala
+++ src/main/scala/io/liquirium/bot/simulation/package.scala
@@ -13,24 +13,27 @@ import io.liquirium.connect.binance.{exchangeId => binanceExchangeId}
 package object simulation {
 
   def singleMarketVisualizationLogger(
-    bot: SingleMarketStrategyBot,
+    market: Market,
+    startTime: Instant,
     additionalCandleStartMetrics: Map[String, Eval[BigDecimal]] = Map.empty,
     additionalCandleEndMetrics: Map[String, Eval[BigDecimal]] = Map.empty,
+    basicCandleLength: Duration,
+    aggregateCandleLength: Duration,
   ): VisualizationLogger = {
-    val market = bot.runConfiguration.market
-    val startTime = bot.runConfiguration.startTime
-    val strategy = bot.strategy
+//    val startTime = bot.runConfiguration.startTime
     // load more candles since indicators need some candle history
     val candlesForIndicatorsEval =
-      InputEval(CandleHistoryInput(market, strategy.candleLength, startTime.minusSeconds(3600 * 10)))
+      InputEval(CandleHistoryInput(market, basicCandleLength, startTime.minusSeconds(3600 * 10)))
 
     val lastPriceEval = candlesForIndicatorsEval.map(_.lastPrice.get)
 
     val visualizationCandlesEval =
       CandleAggregationFold.aggregate(
-        InputEval(CandleHistoryInput(market, strategy.candleLength, startTime)),
-        strategy.candleLength multipliedBy (12 * 4),
+        InputEval(CandleHistoryInput(market, basicCandleLength, startTime)),
+        aggregateCandleLength,
       )
+
+    val tradeHistoryEval = InputEval(TradeHistoryInput(market, startTime))
 
     VisualizationLogger(
       candlesEval = visualizationCandlesEval,
@@ -40,7 +43,7 @@ package object simulation {
         "highestBuy" -> HighestBuyEval(market, fallback = lastPriceEval),
       ) ++ additionalCandleStartMetrics,
       candleEndEvals = Map(
-        "ownTradeVolume" -> LatestCandleTradeVolumeEval(visualizationCandlesEval, bot.tradeHistoryEval),
+        "ownTradeVolume" -> LatestCandleTradeVolumeEval(visualizationCandlesEval, tradeHistoryEval),
       ) ++ additionalCandleEndMetrics,
     )
   }
